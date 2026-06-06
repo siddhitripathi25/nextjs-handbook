d52b776392d5f261c4cd530b512479953bf4132d
Siddhi Tripathi <siddhitripathi104@gmail.com>
2026-06-06 22:25:03 +0530

    Made Banner section

---
+++ class-2/app/components/home-components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <section className="bg-[url('https://www.ideal.in/wp-content/uploads/2023/09/3BHK-flats-website-banner.jpg')]  bg-cover bg-center bg-no-repeat h-screen w-[100%]">
+        <h1 className = "text-4xl font-bold text-black text-center mt-20">Live Above It All - The Pinnacle by</h1>
+        <br />
+        <h1 className = "text-4xl font-bold text-black text-center">STJ Group, Mohali</h1>
+      </section>
+
+    </div>
+  )
+}
+
+export default Banner
--- class-2/app/page.js
+++ class-2/app/page.js
@@ -1,8 +1,9 @@
 import "./globals.css";
+import Banner from "./components/home-components/Banner";
 export default function Home() {
   return (
     <div>
-    
+      <Banner />
     </div>
 
   );
