aab82c7c6fc4a666ad17b96e697109ada3246087
Siddhi Tripathi <siddhitripathi104@gmail.com>
2026-06-07 18:41:39 +0530

    Metadata (SEO Factor)

--- class-2/app/page.js
+++ class-2/app/page.js
@@ -1,7 +1,12 @@
 import "./globals.css";
 import Banner from "./components/home-components/Banner";
 import LatestProduct from "./components/home-components/LatestProduct";
+export const metadata = {
+  
+};
 export default function Home() {
+  metadata.title = "Home Page";
+  metadata.description = "This is the home page of our Next.js application.";
   return (
     <div>
       <Banner />
@@ -10,3 +15,7 @@ export default function Home() {
 
   );
 }
+
+////Similarly, WE CAN GIVE METADATA TO OTHER PAGES AS WELL BY CHANGING THE TITLE AND DESCRIPTION ACCORDING TO THE PAGE.
+// AND THIS METADATA WILL BE SHOWN IN THE BROWSER TAB AND ALSO IN THE SEARCH ENGINE RESULTS.
+///BUT IT WILL WORK ONLY FOR SERVER SIDE RENDERING...IF WE USED 'USE CLIENT' THEN IT WILL NOT WORK BECAUSE IT IS CLIENT SIDE RENDERING AND IN CLIENT SIDE RENDERING THE METADATA IS NOT SHOWN IN THE BROWSER TAB AND ALSO IN THE SEARCH ENGINE RESULTS.
